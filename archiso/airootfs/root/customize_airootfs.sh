96872e1efd51554ff2cd3f11c8be0e3ca542ab7c
3top1a <3top1a.official@gmail.com>
2022-05-03 19:46:27 +0200

    zsh???

--- archiso/airootfs/root/customize_airootfs.sh
+++ archiso/airootfs/root/customize_airootfs.sh
@@ -1,3 +1,9 @@
+
+# Zsh
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+cp -r /root/.oh-my-zsh /avery/.oh-my-zsh
+cp -r /root/.zshrc     /avery/.zshrc
+
 systemctl enable NetworkManager
 systemctl disable ModemManager
 
