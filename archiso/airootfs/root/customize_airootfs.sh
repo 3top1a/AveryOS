1948af5eca94bbf8f4b945312967429002a1694f
3top1a <3top1a.official@gmail.com>
2022-05-03 22:22:34 +0200

    zsh, working statsus

--- archiso/airootfs/root/customize_airootfs.sh
+++ archiso/airootfs/root/customize_airootfs.sh
@@ -1,11 +1,15 @@
-
 # Zsh
+export CHSH="no"
+export RUNZSH="no"
+export KEEP_ZSHRC="yes"
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-cp -r /root/.oh-my-zsh /avery/.oh-my-zsh
-cp -r /root/.zshrc     /avery/.zshrc
+
+cd
+cp -r .oh-my-zsh /home/avery/.oh-my-zsh
 
 systemctl enable NetworkManager
 systemctl disable ModemManager
+systemctl disable bluetooth #Since I don't always need bluetooth
 
 # ly dm
 git clone --recurse-submodules https://github.com/nullgemm/ly.git
@@ -13,6 +17,7 @@ cd ly
 make
 make install
 systemctl enable ly.service
+usermod -aG autologin avery
 
 cd /home/avery/st
 make
