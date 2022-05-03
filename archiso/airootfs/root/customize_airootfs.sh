92734408cced9e888d1447fe5a19328e3c72eb78
3top1a <3top1a.official@gmail.com>
2022-05-03 18:53:05 +0200

    Big poggress

--- archiso/airootfs/root/customize_airootfs.sh
+++ archiso/airootfs/root/customize_airootfs.sh
@@ -1,6 +1,23 @@
+systemctl enable NetworkManager
+systemctl disable ModemManager
+
 # ly dm
 git clone --recurse-submodules https://github.com/nullgemm/ly.git
 cd ly
 make
 make install
 systemctl enable ly.service
+
+cd /home/avery/st
+make
+make install
+
+cd /home/avery/surf
+make
+make install
+
+cd /home/avery
+git clone https://git.suckless.org/tabbed
+cd tabbed
+make
+make install
