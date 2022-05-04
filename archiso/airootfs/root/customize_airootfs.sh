ff6b9a010ac4d1cce052c60efa7c7d2d200d0211
3top1a <3top1a.official@gmail.com>
2022-05-04 16:31:49 +0200

    zsh 2.0

--- archiso/airootfs/root/customize_airootfs.sh
+++ archiso/airootfs/root/customize_airootfs.sh
@@ -7,6 +7,12 @@ export RUNZSH="no"
 export KEEP_ZSHRC="yes"
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
+git clone https://github.com/agkozak/zsh-z ~/.oh-my-zsh/plugins/zsh-z
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
 cd
 cp -r .oh-my-zsh /home/avery/.oh-my-zsh
 
@@ -42,5 +48,3 @@ cd /home/avery
 sudo -u avery git clone https://aur.archlinux.org/yay.git /home/avery/yay
 cd /home/avery/yay
 sudo -u avery makepkg -si
-
-
